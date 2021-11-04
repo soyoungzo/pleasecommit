fd1b74360c36ebe6c51910e5592d59a62d2b0621
soyoungzo <SOYOUNG@josoyoung-ui-MacBook-Pro-4.local>
2021-11-04 16:05:42 +0900

    maintitle

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const eachHeight = () => {
+/*const eachHeight = () => {
     let windowHeight = window.innerHeight;
     document.getElementById("wraper").style.height = String(windowHeight) + "px";
     console.log()
@@ -7,12 +7,12 @@ eachHeight()
 
 window.addEventListener("resize",function(){
     eachHeight()
-});
+});*/
 
 /*flip card effect*/
-const card = document.querySelectorAll(".cardsingle");
+const cards = document.querySelectorAll(".cardsingle");
 
 function flipCard() {
   this.classList.toggle("flip");
 }
-card.forEach((card) => card.addEventListener("click", flipCard))
+cards.forEach((card) => card.addEventListener("click", flipCard))
