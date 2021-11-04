854fe9df7c9c14a8e5301016b50700897fd85e36
Songminseon <smsun0329@naver.com>
2021-11-05 02:36:08 +0900

    케이크워크, 어바웃, 포트폴리오,골드

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 let windowHeight = window.innerHeight;
 
 const eachHeight = () => {
-    document.getElementById("wraper").style.height = String(windowHeight) + "px";
-}
-eachHeight()
+  document.getElementById("wraper").style.height = String(windowHeight) + "px";
+};
+eachHeight();
 
 const innerHeight = () => {
-    const gnb = document.querySelector(".gnb").clientHeight
-    document.querySelector(".main_title").style.height = String(windowHeight)-gnb + "px";
-}
-innerHeight()
+  const gnb = document.querySelector(".gnb").clientHeight;
+  document.querySelector(".main_title").style.height =
+    String(windowHeight) - gnb + "px";
+};
+innerHeight();
 
-window.addEventListener("resize",function(){
-    eachHeight()
-    innerHeight()
+window.addEventListener("resize", function () {
+  eachHeight();
+  innerHeight();
 });
 
 /*flip card effect*/
@@ -22,4 +23,4 @@ const cards = document.querySelectorAll(".cardsingle");
 function flipCard() {
   this.classList.toggle("flip");
 }
-cards.forEach((card) => card.addEventListener("click", flipCard))
+cards.forEach((card) => card.addEventListener("click", flipCard));
